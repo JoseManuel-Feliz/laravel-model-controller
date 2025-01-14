c3013b9b165955c8ef60500a64deb2d29cbc9ee4
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2025-01-14 13:20:56 +0100

    feat: project update

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -9,13 +9,19 @@
 <div>
     <h3> Title: {{$movie['title']}} </h3>
 
-    <img src="{{$movie['url_image']}}" alt="{{$movie['title']}}" <p>Original Title:{{$movie['original_title']}}</p>
+    <img src="{{$movie['url_image']}}" alt="{{$movie['title']}}">
+    <p>Original Title:{{$movie['original_title']}}</p>
     <p>Nationality: {{$movie['nationality']}}</p>
     <p>Release Date: {{$movie['date']}}</p>
     <p>Rating: {{$movie['vote']}}</p>
 </div>
 
 @empty
+<div>
+
+    <h1>sorry no movies available for you!</h1>
+    <img src="https://i1.sndcdn.com/artworks-000638208613-e48vpw-t1080x1080.jpg" alt="">
+</div>
 
 
 @endforelse
